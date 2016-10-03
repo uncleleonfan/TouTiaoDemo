2940b6e1a4e76624bbf451ef5caffcab11992e61
Leon <526247082@qq.com>
2016-10-03 23:49:04 +0800

    Control VideoView.

--- app/src/main/java/com/itheima/toutiaodemo/MainActivity.java
+++ app/src/main/java/com/itheima/toutiaodemo/MainActivity.java
@@ -2,6 +2,8 @@ package com.itheima.toutiaodemo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,5 +19,19 @@ public class MainActivity extends AppCompatActivity {
         mVideoView = (VideoView) findViewById(R.id.video_view);
         mVideoView.setVideoPath(url);
         mVideoView.start();
+    }
+
+    public void onScrollBy(View view) {
+        mVideoView.scrollBy(-100, 0);
+    }
+
+    public void onTranslate(View view) {
+        mVideoView.setTranslationX(100);
+    }
+
+    public void onStartAnimation(View view) {
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, RotateAnimation.RELATIVE_TO_SELF, 0.5f,  RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        rotateAnimation.setDuration(1000);
+        mVideoView.startAnimation(rotateAnimation);
     }
 }
