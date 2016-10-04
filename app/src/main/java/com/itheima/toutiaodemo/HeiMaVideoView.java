35794d95505a9c4545406f588d0d7d715bdce4d7
Leon <526247082@qq.com>
2016-10-04 10:57:07 +0800

    Create HeimaVideoView inherited from TextureView.

---
+++ app/src/main/java/com/itheima/toutiaodemo/HeiMaVideoView.java
@@ -0,0 +1,84 @@
+
+package com.itheima.toutiaodemo;
+
+import android.content.Context;
+import android.graphics.SurfaceTexture;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.util.AttributeSet;
+import android.view.Surface;
+import android.view.TextureView;
+
+import java.io.IOException;
+
+/**
+ * 创建者: Leon
+ * 创建时间: 2016/10/4 10:19
+ * 描述： TODO
+ */
+public class HeiMaVideoView extends TextureView {
+    private static final String TAG = "HeiMaVideoView";
+
+    private MediaPlayer mMediaPlayer;
+    private String mUrl;
+
+    public HeiMaVideoView(Context context) {
+        this(context, null);
+    }
+
+    public HeiMaVideoView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setSurfaceTextureListener(mSurfaceTextureListener);
+    }
+
+    private SurfaceTextureListener mSurfaceTextureListener = new SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            if (mMediaPlayer == null) {
+                mMediaPlayer = new MediaPlayer();
+                mMediaPlayer.setOnPreparedListener(mOnPreparedListener);
+                mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+                mMediaPlayer.setScreenOnWhilePlaying(true);
+                mMediaPlayer.setSurface(new Surface(surface));
+            }
+            try {
+                mMediaPlayer.setDataSource(getContext(), Uri.parse(mUrl));
+                mMediaPlayer.prepareAsync();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            mMediaPlayer.release();
+            return true;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    public void setVideoPath(String url) {
+        mUrl = url;
+    }
+
+    private MediaPlayer.OnPreparedListener mOnPreparedListener = new MediaPlayer.OnPreparedListener() {
+        @Override
+        public void onPrepared(MediaPlayer mp) {
+            mMediaPlayer.start();
+        }
+    };
+
+
+
+
+}
--- app/src/main/java/com/itheima/toutiaodemo/MainActivity.java
+++ app/src/main/java/com/itheima/toutiaodemo/MainActivity.java
@@ -1,22 +1,26 @@
 package com.itheima.toutiaodemo;
 
-import android.animation.ObjectAnimator;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.animation.RotateAnimation;
 import android.widget.VideoView;
 
 public class MainActivity extends AppCompatActivity {
 
     private VideoView mVideoView;
 
+    private HeiMaVideoView mHeiMaVideoView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mVideoView = (VideoView) findViewById(R.id.video_view);
-        mVideoView.setVideoPath(Constant.url);
-        mVideoView.start();
+        mHeiMaVideoView = (HeiMaVideoView) findViewById(R.id.heima_video_view);
+//        mVideoView.setVideoPath(Constant.url);
+//        mVideoView.start();
+        mHeiMaVideoView.setVideoPath(Constant.url);
     }
 
     public void onScrollBy(View view) {
@@ -28,11 +32,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStartAnimation(View view) {
-/*        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, RotateAnimation.RELATIVE_TO_SELF, 0.5f,  RotateAnimation.RELATIVE_TO_SELF, 0.5f);
+        RotateAnimation rotateAnimation = new RotateAnimation(0, 360, RotateAnimation.RELATIVE_TO_SELF, 0.5f, RotateAnimation.RELATIVE_TO_SELF, 0.5f);
         rotateAnimation.setDuration(1000);
-        mVideoView.startAnimation(rotateAnimation);*/
-        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(mVideoView, "rotation", 0f, -180f);
+//        mVideoView.startAnimation(rotateAnimation);
+        mHeiMaVideoView.startAnimation(rotateAnimation);
+/*        ObjectAnimator objectAnimator = ObjectAnimator.ofFloat(mVideoView, "rotation", 0f, -180f);
         objectAnimator.setDuration(1000);
-        objectAnimator.start();
+        objectAnimator.start();*/
     }
 }
